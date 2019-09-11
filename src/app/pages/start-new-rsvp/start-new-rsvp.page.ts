5a8008ccae84538e16dd715f7bc3368bffb2785a
jonathan laroco <jonathanlaroco@jonathans-MacBook-Pro.local>
2019-09-10 22:28:23 -0700

    Ad this

--- src/app/pages/start-new-rsvp/start-new-rsvp.page.ts
+++ src/app/pages/start-new-rsvp/start-new-rsvp.page.ts
@@ -154,10 +154,10 @@ export class StartNewRsvpPage implements OnInit {
                 this.rsvpGuest.Name = data[k];
                 this.rsvpGuestService.addRsvpGuest(this.rsvpGuest).then(docRef => {
                   this.rsvpGuest.id = docRef.id;
-                  this.rsvpAttending.id = this.rsvpGuest.Name + '-' + k;
+                  this.rsvpAttending.id = data[k] + '-' + k;
                   this.rsvpAttending.rsvpID = this.getRsvp.id;
                   this.rsvpAttending.rsvpGuestID = '';
-                  this.rsvpAttend.addRsvpAttending(this.rsvpAttending,this.rsvpGuest.Name + '-' + k);
+                  this.rsvpAttend.addRsvpAttending(this.rsvpAttending,data[k] + '-' + k);
                 });
               }
             } 
