efe7c79c4908360c47843b41003e9cc8003e5cda
Jonathan Laroco <jonathan.laroco@gmail.com>
2019-09-11 11:48:55 -0700

    lets try this.

--- src/app/pages/start-new-rsvp/start-new-rsvp.page.ts
+++ src/app/pages/start-new-rsvp/start-new-rsvp.page.ts
@@ -166,13 +166,13 @@ export class StartNewRsvpPage implements OnInit {
     this.alertController.create(options).then(alert => alert.present());
   }
 
-  /*getAllGuests() {
+  getAllGuests() {
     this.events.publish('guest:created', this.getRsvp.id);
     var rsvpGuestUbsubscribe = this.rsvpGuestService.getRsvpGuestsForSearch().subscribe(data => {
       this.addRsvpGuests  = data;
       rsvpGuestUbsubscribe.unsubscribe();
     })
-  }*/
+  }
 
  askDietaryRestrictions() {
     this.alertController.create({
