b82a1841a60ab1c6144e3c1e73f43cc11bb4b9b2
jonathan laroco <jonathanlaroco@jonathans-MacBook-Pro.local>
2019-09-11 19:52:43 -0700

    lets try this.

--- src/app/pages/rsvp-diet-restrictions/rsvp-diet-restrictions.page.ts
+++ src/app/pages/rsvp-diet-restrictions/rsvp-diet-restrictions.page.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { RsvpGuest, RsvpGuestService } from 'src/app/services/rsvp-guest.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Events } from 'ionic-angular';
 
 @Component({
   selector: 'app-rsvp-diet-restrictions',
   templateUrl: './rsvp-diet-restrictions.page.html',
   styleUrls: ['./rsvp-diet-restrictions.page.scss'],
 })
-export class RsvpDietRestrictionsPage implements OnInit {
+export class RsvpDietRestrictionsPage {
+  rsvpGuests: RsvpGuest[];
+  rsvpId = null;
 
-  constructor() { }
+  constructor(
+    private rsvpGuestService: RsvpGuestService,
+    private route: ActivatedRoute,
+    private router: Router,
+    public events: Events
+  ) { }
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.getEventData();
+  }
+
+  getEventData() {
+    this.rsvpId = this.route.snapshot.params['id'];
+    if (this.rsvpId)  {   
+      this.events.publish('guest:created', this.rsvpId);  
+      this.rsvpGuestService.getRsvpGuests()
+      .then(data => {
+        this.rsvpGuests = data;
+      }) 
+    }
   }
 
   submitDietRestrictions() {
-    
+    console.log("SUBMIT THAT!!!");
   }
 
 }
