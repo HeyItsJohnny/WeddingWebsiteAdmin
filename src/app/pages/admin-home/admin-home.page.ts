02f9b3b42367b3edf1152e188c08f93ed581f097
jonathan laroco <jonathanlaroco@jonathans-MacBook-Pro.local>
2019-09-15 16:50:54 -0700

    change

--- src/app/pages/admin-home/admin-home.page.ts
+++ src/app/pages/admin-home/admin-home.page.ts
@@ -68,7 +68,9 @@ export class AdminHomePage {
     });
 
     guestData.addRow("Attending", Attending);
+    chart.draw(guestData, options);
     guestData.addRow(["Not Attending",NotAttending]);
+    chart.draw(guestData, options);
 
     var options = {
       title: 'Wedding Attendance'
