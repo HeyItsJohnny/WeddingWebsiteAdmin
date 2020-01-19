c382159218524cbd707793e6592e3c22dfc655f2
jonathan laroco <jonathanlaroco@jonathans-MacBook-Pro.local>
2020-01-18 19:33:00 -0800

    lets try this.

--- src/app/pages/wedding-guests/wedding-guests.page.ts
+++ src/app/pages/wedding-guests/wedding-guests.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RsvpGuest, RsvpGuestService } from 'src/app/services/rsvp-guest.service';
+import { Rsvp, RsvpService } from 'src/app/services/rsvp.service';
+
 
 @Component({
   selector: 'app-wedding-guests',
   templateUrl: './wedding-guests.page.html',
   styleUrls: ['./wedding-guests.page.scss'],
 })
-export class WeddingGuestsPage implements OnInit {
+export class WeddingGuestsPage {
+  public rsvps: Array<any>;
+  public rsvpGuests: Array<any>;
+  public guests: Array<any>;
 
-  constructor() { }
+  constructor(
+    private rsvpGuestService: RsvpGuestService,
+    private rsvpService: RsvpService) { }
 
-  ngOnInit() {
-  }
+ 
+    ionViewWillEnter() {
+      this.getRsvpData();
+      this.getRsvpGuests();
+    }
+
+    getRsvpData() {
+      this.rsvpService.getRsvps().then(data => {
+        this.rsvps = data;
+      });
+    }
+
+    getRsvpGuests() {
+      for(let item of this.rsvps) {
+        console.log('ID: ' + item.payload.doc.id + ' Name: ' + item.payload.doc.data().Name); 
+      }
+    }
 
 }
